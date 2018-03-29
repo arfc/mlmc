0c96c79453d6233f8d5abe20a78641025ec69ccc
Caitlin Wen <caitlinwen@CaitlindeMacBook-Pro.local>
2018-03-29 15:40:20 -0500

    modify parse

--- parse.py
+++ parse.py
@@ -3,17 +3,16 @@ import h5py
 
 filename = sys.argv[-1]
 try:
-    f = h5py.File(filename, 'r+')
+    f = h5py.File(filename, 'r')
 except:
     print("there is no such file")
     exit()
 # List all groups
-print("Keys: %s" % f.keys())
-#a_group_key = list(f.keys())[0]
+datasetNames = [n for n in f.keys()]
+for n in datasetNames:
+	print(n)
 
 # Get the data
-#data = list(f[a_group_key])
-
-
-#print(data)
-
+global_tallies = f['global_tallies']
+global_tallies_data = global_tallies[:]
+print(global_tallies_data)
